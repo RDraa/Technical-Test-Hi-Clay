81646ef1f098e9b746d50db3ff05a2eb3f52d4e8
Muhammad Ruswendra <muhammadruswendra@gmail.com>
2025-05-28 14:08:01 +0700

    Movement, jump

---
+++ Assets/Script/MovementController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float jumpForce = 4f;
+    [SerializeField] private float fallMultiplier = 2.5f;
+    [SerializeField] private float lowJumpMultiplier = 2f;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+
+    [SerializeField] private Rigidbody2D body;
+
+    [SerializeField] private Transform groundCheck;
+
+    [SerializeField] private LayerMask floor;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        MController();
+    }
+
+    private void MController()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        // float airSpeed = 0.1f;
+
+        transform.Translate(new Vector3(horizontalInput, 0, 0) * moveSpeed * Time.deltaTime);
+
+
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-0.6f, 0.6f, 0.6f);
+        }
+
+        // if (!IsGrounded())
+        // {
+        //     float clampedX = Mathf.Clamp(body.velocity.x, -airSpeed, airSpeed);
+        //     body.velocity = new Vector2(clampedX, body.velocity.y);
+        // }
+
+        Jump();
+        Gravity();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+            Debug.Log("Jump!");
+        }
+    }
+
+    private void Gravity()
+    {
+        if (body.velocity.y < 0)
+        {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (body.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, floor);
+    }
+}
