c5aa0235c2cdb4838aad42ab7cce9cc328cd1d72
Muhammad Ruswendra <muhammadruswendra@gmail.com>
2025-05-29 05:22:49 +0700

    Camera Follow

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform characterPosition;
+    public GameObject characterObject;
+
+    public Vector3 offset;
+    [Range(1, 10)]
+    public float smoothFactor;
+    public Vector3 maxValue, minValue;
+
+    void FixedUpdate()
+    {
+        FollowTarget(characterPosition);
+    }
+
+    void FollowTarget(Transform target)
+    {
+        if (target == null) return;
+
+        Vector3 targetPosition = characterPosition.position + offset;
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValue.x, maxValue.x),
+            Mathf.Clamp(targetPosition.y, minValue.y, maxValue.y),
+            Mathf.Clamp(targetPosition.z, minValue.z, maxValue.z));
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = smoothPosition;
+    }
+}
